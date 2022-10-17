44819e81ca3375710c27c38d47f5824cb1f171a7
yauheni.prakapenka <>
2022-10-17 22:51:09 +0300

    Add amount tests

--- test/examples/amount_test.dart
+++ test/examples/amount_test.dart
@@ -1,51 +1,96 @@
 import 'package:test/test.dart';
 
 void main() {
-  const String pattern = r'^[0-9]+\.?[0-9]*?$';
+  group('Amount pattern', () {
+    final RegExp pattern = RegExp(r'^[0-9]+\.?[0-9]*?$');
 
-  group('Match', () {
-    test('Tests', () {
-      final bool actual1 = RegExp(pattern).hasMatch('0');
-      final bool actual2 = RegExp(pattern).hasMatch('1');
-      final bool actual3 = RegExp(pattern).hasMatch('9');
-      final bool actual4 = RegExp(pattern).hasMatch('777');
-      final bool actual5 = RegExp(pattern).hasMatch('0.1');
-      final bool actual6 = RegExp(pattern).hasMatch('10.01');
-      final bool actual7 = RegExp(pattern).hasMatch('000.111');
-      final bool actual8 = RegExp(pattern).hasMatch('0.0123456789');
-      final bool actual9 = RegExp(pattern).hasMatch('1.');
-      expect(actual1, true);
-      expect(actual2, true);
-      expect(actual3, true);
-      expect(actual4, true);
-      expect(actual5, true);
-      expect(actual6, true);
-      expect(actual7, true);
-      expect(actual8, true);
-      expect(actual9, true);
+    group('Matches', () {
+      test('0', () {
+        final bool actual = pattern.hasMatch('0');
+        expect(actual, true);
+      });
+
+      test('1', () {
+        final bool actual = pattern.hasMatch('1');
+        expect(actual, true);
+      });
+
+      test('9', () {
+        final bool actual = pattern.hasMatch('9');
+        expect(actual, true);
+      });
+
+      test('777', () {
+        final bool actual = pattern.hasMatch('777');
+        expect(actual, true);
+      });
+
+      test('0.1', () {
+        final bool actual = pattern.hasMatch('0.1');
+        expect(actual, true);
+      });
+
+      test('10.01', () {
+        final bool actual = pattern.hasMatch('10.01');
+        expect(actual, true);
+      });
+
+      test('000.111', () {
+        final bool actual = pattern.hasMatch('000.111');
+        expect(actual, true);
+      });
+
+      test('0.0123456789', () {
+        final bool actual = pattern.hasMatch('0.0123456789');
+        expect(actual, true);
+      });
+
+      test('1.', () {
+        final bool actual = pattern.hasMatch('1.');
+        expect(actual, true);
+      });
     });
-  });
 
-  group('Not match', () {
-    test('Tests', () {
-      final bool actual1 = RegExp(pattern).hasMatch('.1');
-      final bool actual2 = RegExp(pattern).hasMatch('00..');
-      final bool actual3 = RegExp(pattern).hasMatch('..00');
-      final bool actual4 = RegExp(pattern).hasMatch('1..0');
-      final bool actual5 = RegExp(pattern).hasMatch('!');
-      final bool actual6 = RegExp(pattern).hasMatch('=');
-      final bool actual7 = RegExp(pattern).hasMatch('+');
-      final bool actual8 = RegExp(pattern).hasMatch(r'$');
-      final bool actual9 = RegExp(pattern).hasMatch('.');
-      expect(actual1, false);
-      expect(actual2, false);
-      expect(actual3, false);
-      expect(actual4, false);
-      expect(actual5, false);
-      expect(actual6, false);
-      expect(actual7, false);
-      expect(actual8, false);
-      expect(actual9, false);
+    group('Not matches', () {
+      test('.1', () {
+        final bool actual = pattern.hasMatch('.1');
+        expect(actual, false);
+      });
+
+      test('00..', () {
+        final bool actual = pattern.hasMatch('00..');
+        expect(actual, false);
+      });
+
+      test('..00', () {
+        final bool actual = pattern.hasMatch('..00');
+        expect(actual, false);
+      });
+
+      test('1..0', () {
+        final bool actual = pattern.hasMatch('1..0');
+        expect(actual, false);
+      });
+
+      test('!', () {
+        final bool actual = pattern.hasMatch('!');
+        expect(actual, false);
+      });
+
+      test('=', () {
+        final bool actual = pattern.hasMatch('=');
+        expect(actual, false);
+      });
+
+      test(r'$', () {
+        final bool actual = pattern.hasMatch(r'$');
+        expect(actual, false);
+      });
+
+      test('.', () {
+        final bool actual = pattern.hasMatch('.');
+        expect(actual, false);
+      });
     });
   });
 }
